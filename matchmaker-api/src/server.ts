bc84e9828ed02e52994411cf973d9260b0010113
PINGUET Julien <pinguet62@gmail.com>
2018-02-12 13:40:48 +0100

    Version upgrade

--- matchmaker-api/src/server.ts
+++ matchmaker-api/src/server.ts
@@ -1,4 +1,4 @@
-import * as bodyParser from "body-parser";
+import { json } from "body-parser";
 import * as cors from "cors";
 import * as express from "express";
 import {Application} from "express";
@@ -13,7 +13,7 @@ import {registerRoutes} from "./router";
     const app: Application = express();
 
     app.use(cors());
-    app.use(bodyParser.json());
+    app.use(json());
     registerRoutes(app);
     app.use(exceptionHandler);
 
