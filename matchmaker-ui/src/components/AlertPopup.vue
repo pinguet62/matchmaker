449981dabfc29fe8bed70e7dc4c4c6b3f9d89013
PINGUET Julien <pinguet62@gmail.com>
2018-02-15 11:45:41 +0100

    UI: Global pop-up component

---
+++ matchmaker-ui/src/components/AlertPopup.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-snackbar v-model="show" :timeout="500" :top="true" :vertical="true">
+    <h1>{{title}}</h1>
+    <p>{{details}}</p>
+  </v-snackbar>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export const AlertPopupBus = new Vue()
+
+  export default {
+    data () {
+      return {
+        show: false,
+        title: null,
+        details: null
+      }
+    },
+    created () {
+      AlertPopupBus.$on('display', data => {
+        this.title = data.title
+        this.details = data.details
+        this.show = true
+      })
+    }
+  }
+</script>
